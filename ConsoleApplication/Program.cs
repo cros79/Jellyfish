62d77c4aa0ae8f225afc57d04ba43101f8c37dab
Christoffer Rosenqvist <christoffer.rosenqvist@invativa.se>
2014-03-24 21:58:03 +0100

    Added an implementing console application. Runs just fine.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApplication {
+    using System;
+    using InvativaNet.Core.Utilities;
+
+    public class Program {
+        private static void Main(string[] args) {
+            Console.WriteLine(General.JsonSerialize(new {Hejkon = "Bacon"}));
+            Console.ReadLine();
+        }
+    }
+}
